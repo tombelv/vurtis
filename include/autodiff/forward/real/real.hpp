6bc9d6ac2dd161d29f51412c42f9a14704a19e52
tombelv <tommaso.belv@gmail.com>
2023-03-15 15:56:11 +0100

    updated autodiff

--- include/autodiff/forward/real/real.hpp
+++ include/autodiff/forward/real/real.hpp
@@ -7,7 +7,7 @@
 //
 // Licensed under the MIT License <http://opensource.org/licenses/MIT>.
 //
-// Copyright (c) 2018-2020 Allan Leal
+// Copyright (c) 2018-2022 Allan Leal
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and `associated documentation files (the "Software"), to deal
@@ -74,7 +74,7 @@ public:
     {}
 
     /// Construct a Real number with given data.
-    template<size_t M, typename U, EnableIf<isArithmetic<U>>...>
+    template<size_t M, typename U, Requires<isArithmetic<U>> = true>
     constexpr explicit Real(const Real<M, U>& other)
     {
         static_assert(N <= M);
@@ -105,7 +105,7 @@ public:
         return m_data[i];
     }
 
-    template<typename U, EnableIf<isArithmetic<U>>...>
+    template<typename U, Requires<isArithmetic<U>> = true>
     constexpr auto operator=(const U& value) -> Real&
     {
         m_data[0] = value;
@@ -119,28 +119,28 @@ public:
         return *this;
     }
 
-    template<typename U, EnableIf<isArithmetic<U>>...>
+    template<typename U, Requires<isArithmetic<U>> = true>
     constexpr auto operator+=(const U& value) -> Real&
     {
         m_data[0] += static_cast<T>(value);
         return *this;
     }
 
-    template<typename U, EnableIf<isArithmetic<U>>...>
+    template<typename U, Requires<isArithmetic<U>> = true>
     constexpr auto operator-=(const U& value) -> Real&
     {
         m_data[0] -= static_cast<T>(value);
         return *this;
     }
 
-    template<typename U, EnableIf<isArithmetic<U>>...>
+    template<typename U, Requires<isArithmetic<U>> = true>
     constexpr auto operator*=(const U& value) -> Real&
     {
         For<0, N + 1>([&](auto i) constexpr { m_data[i] *= static_cast<T>(value); });
         return *this;
     }
 
-    template<typename U, EnableIf<isArithmetic<U>>...>
+    template<typename U, Requires<isArithmetic<U>> = true>
     constexpr auto operator/=(const U& value) -> Real&
     {
         For<0, N + 1>([&](auto i) constexpr { m_data[i] /= static_cast<T>(value); });
@@ -188,10 +188,10 @@ public:
 
     /// Convert this Real number into a value of type @p U.
 #if defined(AUTODIFF_ENABLE_IMPLICIT_CONVERSION_REAL) || defined(AUTODIFF_ENABLE_IMPLICIT_CONVERSION)
-    template<typename U, EnableIf<isArithmetic<U>>...>
+    template<typename U, Requires<isArithmetic<U>> = true>
     constexpr operator U() const { return static_cast<U>(m_data[0]); }
 #else
-    template<typename U, EnableIf<isArithmetic<U>>...>
+    template<typename U, Requires<isArithmetic<U>> = true>
     constexpr explicit operator U() const { return static_cast<U>(m_data[0]); }
 #endif
 };
@@ -207,6 +207,7 @@ using std::acos;
 using std::acosh;
 using std::asin;
 using std::asinh;
+using std::atan2;
 using std::atan;
 using std::atanh;
 using std::cbrt;
@@ -274,13 +275,13 @@ auto operator+(Real<N, T> x, const Real<N, T>& y)
     return x += y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 auto operator+(Real<N, T> x, const U& y)
 {
     return x += y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 auto operator+(const U& x, Real<N, T> y)
 {
     return y += x;
@@ -297,13 +298,13 @@ auto operator-(Real<N, T> x, const Real<N, T>& y)
     return x -= y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 auto operator-(Real<N, T> x, const U& y)
 {
     return x -= y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 auto operator-(const U& x, Real<N, T> y)
 {
     y -= x;
@@ -323,13 +324,13 @@ auto operator*(Real<N, T> x, const Real<N, T>& y)
     return x *= y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 auto operator*(Real<N, T> x, const U& y)
 {
     return x *= y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 auto operator*(const U& x, Real<N, T> y)
 {
     return y *= x;
@@ -347,13 +348,13 @@ auto operator/(Real<N, T> x, const Real<N, T>& y)
     return x /= y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 auto operator/(Real<N, T> x, const U& y)
 {
     return x /= y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 auto operator/(const U& x, Real<N, T> y)
 {
     Real<N, T> z = x;
@@ -485,7 +486,7 @@ constexpr auto pow(const Real<N, T>& x, const Real<N, T>& y)
     return res;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 constexpr auto pow(const Real<N, T>& x, const U& c)
 {
     Real<N, T> res;
@@ -505,7 +506,7 @@ constexpr auto pow(const Real<N, T>& x, const U& c)
     return res;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 constexpr auto pow(const U& c, const Real<N, T>& y)
 {
     Real<N, T> res;
@@ -651,6 +652,60 @@ constexpr auto atan(const Real<N, T>& x)
         aux = xprime/(1 + aux*aux);
         For<1, N + 1>([&](auto i) constexpr {
             res[i] = aux[i - 1];
+        });
+    }
+    return res;
+}
+
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
+constexpr auto atan2(const U& c, const Real<N, T>& x)
+{
+    // d[atan2(c,x)]/dx = -c / (c^2 + x^2)
+    Real<N, T> res;
+    res[0] = atan2(c, x[0]);
+    if constexpr(N > 0) {
+        Real<N - 1, T> xprime;
+        For<1, N + 1>([&](auto i) constexpr {
+            xprime[i - 1] = x[i];
+        });
+        Real<N - 1, T> aux(x);
+        aux = xprime * (-c / (c * c + aux * aux));
+        For<1, N + 1>([&](auto i) constexpr {
+            res[i] = aux[i - 1];
+        });
+    }
+    return res;
+}
+
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
+constexpr auto atan2(const Real<N, T>& y, const U& c)
+{
+    // d[atan2(y,c)]/dy = c / (c^2 + y^2)
+    Real<N, T> res;
+    res[0] = atan2(y[0], c);
+    if constexpr(N > 0) {
+        Real<N - 1, T> yprime;
+        For<1, N + 1>([&](auto i) constexpr {
+            yprime[i - 1] = y[i];
+        });
+        Real<N - 1, T> aux(y);
+        aux = yprime * (c / (c * c + aux * aux));
+        For<1, N + 1>([&](auto i) constexpr {
+            res[i] = aux[i - 1];
+        });
+    }
+    return res;
+}
+
+template<size_t N, typename T>
+constexpr auto atan2(const Real<N, T>& y, const Real<N, T>& x)
+{
+    Real<N, T> res;
+    res[0] = atan2(y[0], x[0]);
+    if constexpr(N > 0) {
+        const T denom = x[0] * x[0] + y[0] * y[0];
+        For<1, N + 1>([&](auto i) constexpr {
+            res[i] = (x[0] * y[i] - x[i] * y[0]) / denom;
         });
     }
     return res;
@@ -806,13 +861,13 @@ constexpr auto min(const Real<N, T>& x, const Real<N, T>& y)
     return (x[0] <= y[0]) ? x : y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 constexpr auto min(const Real<N, T>& x, const U& y)
 {
     return (x[0] <= y) ? x : y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 constexpr auto min(const U& x, const Real<N, T>& y)
 {
     return (x < y[0]) ? x : y;
@@ -824,13 +879,13 @@ constexpr auto max(const Real<N, T>& x, const Real<N, T>& y)
     return (x[0] >= y[0]) ? x : y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 constexpr auto max(const Real<N, T>& x, const U& y)
 {
     return (x[0] >= y) ? x : y;
 }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...>
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true>
 constexpr auto max(const U& x, const Real<N, T>& y)
 {
     return (x > y[0]) ? x : y;
@@ -882,19 +937,19 @@ template<size_t N, typename T> bool operator> (const Real<N, T>& x, const Real<N
 template<size_t N, typename T> bool operator<=(const Real<N, T>& x, const Real<N, T>& y) { return x[0] <= y[0]; }
 template<size_t N, typename T> bool operator>=(const Real<N, T>& x, const Real<N, T>& y) { return x[0] >= y[0]; }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator==(const Real<N, T>& x, const U& y) { return x[0] == y; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator!=(const Real<N, T>& x, const U& y) { return x[0] != y; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator< (const Real<N, T>& x, const U& y) { return x[0] <  y; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator> (const Real<N, T>& x, const U& y) { return x[0] >  y; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator<=(const Real<N, T>& x, const U& y) { return x[0] <= y; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator>=(const Real<N, T>& x, const U& y) { return x[0] >= y; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator==(const Real<N, T>& x, const U& y) { return x[0] == y; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator!=(const Real<N, T>& x, const U& y) { return x[0] != y; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator< (const Real<N, T>& x, const U& y) { return x[0] <  y; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator> (const Real<N, T>& x, const U& y) { return x[0] >  y; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator<=(const Real<N, T>& x, const U& y) { return x[0] <= y; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator>=(const Real<N, T>& x, const U& y) { return x[0] >= y; }
 
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator==(const U& x, const Real<N, T>& y) { return x == y[0]; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator!=(const U& x, const Real<N, T>& y) { return x != y[0]; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator< (const U& x, const Real<N, T>& y) { return x <  y[0]; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator> (const U& x, const Real<N, T>& y) { return x >  y[0]; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator<=(const U& x, const Real<N, T>& y) { return x <= y[0]; }
-template<size_t N, typename T, typename U, EnableIf<isArithmetic<U>>...> bool operator>=(const U& x, const Real<N, T>& y) { return x >= y[0]; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator==(const U& x, const Real<N, T>& y) { return x == y[0]; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator!=(const U& x, const Real<N, T>& y) { return x != y[0]; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator< (const U& x, const Real<N, T>& y) { return x <  y[0]; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator> (const U& x, const Real<N, T>& y) { return x >  y[0]; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator<=(const U& x, const Real<N, T>& y) { return x <= y[0]; }
+template<size_t N, typename T, typename U, Requires<isArithmetic<U>> = true> bool operator>=(const U& x, const Real<N, T>& y) { return x >= y[0]; }
 
 //=====================================================================================================================
 //
